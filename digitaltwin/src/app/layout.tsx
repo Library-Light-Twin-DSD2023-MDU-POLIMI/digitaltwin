4f2ff0ac411a9b8b1cdbf6697bf58b3adcb1d5ba
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2023-11-13 08:36:28 +0100

    Resolving git merge conflicts

--- digitaltwin/src/app/layout.tsx
+++ digitaltwin/src/app/layout.tsx
@@ -1,20 +1,25 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Header from './components/header/header'
 
-const inter = Inter({ subsets: ['latin'] })
+import { ReactNode } from "react";
+import Header from './components/header/header';
 
+type Props = {
+  children: ReactNode
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+const layout = (props: Props) => {
   return (
-    <html lang="en">
-      <body> 
-        <Header/>
-      </body>
-    </html>
+<div>
+<div id="header"><Header /></div>
+<div id="main-wrap">
+  
+  <div id="content-wrap">
+  <main className="">
+        {props.children}
+      </main>
+  </div>
+</div>
+</div>
   )
 }
+
+export default layout
