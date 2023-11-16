58c6b942618010f53d62ae3c94e677c5953a5b05
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2023-11-16 20:09:02 +0100

    Styling to the header

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -6,10 +6,10 @@ import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nex
 
 export default function Header() {
   return (
-    <Navbar>
-      <NavbarBrand>
-        <p className="font-bold text-inherit">Lightning Asset Manager</p>
-      </NavbarBrand>
-    </Navbar>
+    <div className="flex items-center bg-cyan-950 h-14">
+      <button className="bg-gray-400 text-white flex items-center justify-center w-60 h-full rounded-tr-md rounded-br-md shrink-0">
+      </button>
+      <h1 className="text-2xl font-bold text-white flex-grow pl-8 ml-20">Library Lightning Asset Management System</h1>
+    </div>
   );
 }
