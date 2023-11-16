d9074f04dc050f19e595286c3efdfe96c08b8cdc
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2023-11-16 20:08:48 +0100

    Plain dashboard implementation (only text)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,10 @@
 import Image from 'next/image'
 
-import { Button } from '@nextui-org/button'
-import { Card, CardBody } from '@nextui-org/card'
-
-
 export default function Home() {
   return (
-    <section className='py-36'>
-      <div className='container flex items-center justify-center'>
-        Here comes the dashboard
-      </div>
-    </section>
+    <div>
+      Here comes the dashboard
+    </div>
+      
   )
 }
