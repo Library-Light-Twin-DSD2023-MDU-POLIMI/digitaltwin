d63fe5e08d53fa9dbd40dc0d55f65e76b3c0d1b0
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2023-11-28 09:53:54 +0100

    Fix: fixed merge conflicts regarding sidebar

--- frontend/src/app/components/sidebar.tsx
+++ frontend/src/app/components/sidebar.tsx
@@ -28,7 +28,7 @@ export default function Sidebar() {
 
   return (
     <div id="sidebar">
-      <div className='w-60 min-h-full'>
+      <div className='w-60 h-full min-h-full'>
         <div className='flex grow flex-col h-full gapy-y-5 overflow-y-auto bg-cyan-950 bg-blend-darken px-4 pb-4 border-r-2' style={{ minHeight: '100vh' }}>
           <nav className='flex flex-1 flex-col'
           data-te-sidenav-init
