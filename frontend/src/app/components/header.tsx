8a545242b26f04abeeaa54ef703e151764061eb2
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2023-11-28 09:52:21 +0100

    Fix: Fixed git merge conflict with header component

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -1,17 +1,15 @@
 'use client'
 
 import React from "react";
-import {Navbar, NavbarItem} from "@nextui-org/react";
-import { HomeIcon } from "@heroicons/react/24/solid";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
 
 
 export default function Header() {
   return (
-    
-      <div className="flex items-center bg-cyan-950 h-14">
-        <button className="text-white flex items-center justify-center w-60 h-full shrink-0">
-        </button>
-        <h1 className="text-2xl font-bold text-white flex-grow pl-8 ml-20">Library Lightning Asset Management System</h1>
-      </div>
+    <div className="flex items-center bg-cyan-950 h-14">
+      <button className="text-white flex items-center justify-center w-60 h-full shrink-0">
+      </button>
+      <h1 className="text-2xl font-bold text-white flex-grow pl-8 ml-20">Library Lightning Asset Management System</h1>
+    </div>
   );
 }
