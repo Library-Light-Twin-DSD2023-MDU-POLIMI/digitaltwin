964bc3f02dacc8e7fa888d9eb6862ae51e219e02
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2023-12-08 15:52:21 +0100

    Feat: Added queries matching the new schema

---
+++ frontend/src/app/_GraphQL/Queries.ts
@@ -0,0 +1,166 @@
+import { gql } from '@apollo/client';
+
+export const GET_LIGHTING_ASSET = gql`
+  query GetLightingAsset($id: ID!) {
+    lightingAsset(id: $id) {
+      _id
+      uid
+      currentStatus
+      predictiveStatus {
+        status
+        predictedTime
+      }
+      type
+      location {
+        floor
+        section
+        area
+      }
+      workOrders {
+        _id
+        workOrderID
+        lightingAssetID
+        type
+        workOrderStatus
+        description
+        comment
+        location {
+          floor
+          section
+          area
+        }
+        dateOfMaintenance
+        executionStartDate
+        executedDate
+      }
+      cilLevel
+    }
+  }
+`;
+
+export const GET_LIGHTING_ASSETS = gql`
+  query GetLightingAssets($input: SortAndPaginate, $filter: LightingAssetFilter) {
+    lightingAssets(input: $input, filter: $filter) {
+      _id
+      uid
+      currentStatus
+      predictiveStatus {
+        status
+        predictedTime
+      }
+      type
+      location {
+        floor
+        section
+        area
+      }
+      cilLevel
+    }
+  }
+`;
+
+export const GET_LIGHTING_ASSET_TIME_SERIES_DATA = gql`
+  query GetLightingAssetTimeSeriesData($assetId: ID!, $startTime: String!, $endTime: String!, $thresholds: TimeSeriesDataThresholds) {
+    getLightingAssetTimeSeriesData(assetId: $assetId, startTime: $startTime, endTime: $endTime, thresholds: $thresholds) {
+      timestamp
+      assetId
+      power {
+        WATT {
+          value
+          healthStatus
+        }
+      }
+      // ... other fields as needed
+    }
+  }
+`;
+
+export const GET_AVERAGE_LIGHTING_ASSET_DATA = gql`
+  query GetAverageLightingAssetData($assetId: ID!, $startTime: String!, $endTime: String!) {
+    getAverageLightingAssetData(assetId: $assetId, startTime: $startTime, endTime: $endTime) {
+      timestamp
+      averageIlluminance
+      // ... other fields as needed
+    }
+  }
+`;
+
+export const GET_METRICS = gql`
+  query GetMetrics {
+    metrics {
+      metric
+      unit
+      scale {
+        tooHigh
+        perfect
+        good
+        mid
+        tooLow
+      }
+      information
+      tooltipSummary
+    }
+  }
+`;
+
+export const GET_METRIC = gql`
+  query GetMetric($metric: String!) {
+    metric(metric: $metric) {
+      metric
+      unit
+      scale {
+        tooHigh
+        perfect
+        good
+        mid
+        tooLow
+      }
+      information
+      tooltipSummary
+    }
+  }
+`;
+
+export const GET_WORK_ORDER = gql`
+  query GetWorkOrder($id: ID!) {
+    workOrder(id: $id) {
+      _id
+      workOrderID
+      lightingAssetID
+      type
+      workOrderStatus
+      description
+      comment
+      location {
+        floor
+        section
+        area
+      }
+      dateOfMaintenance
+      executionStartDate
+      executedDate
+    }
+  }
+`;
+
+export const GET_WORK_ORDERS = gql`
+  query GetWorkOrders {
+    workOrders {
+      _id
+      workOrderID
+      lightingAssetID
+      type
+      workOrderStatus
+      description
+      comment
+      location {
+        floor
+        section
+        area
+      }
+      dateOfMaintenance
+      executionStartDate
+      executedDate
+    }
+  }
+`;
