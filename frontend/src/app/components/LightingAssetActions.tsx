3d87aa8765b5fd174130f706ca3a9cf322c9e9f7
heshani-wick <hwe22001@student.mdu.se>
2023-12-09 01:36:23 +0100

    implement report workorder and linked workorder

--- frontend/src/app/components/LightingAssetActions.tsx
+++ frontend/src/app/components/LightingAssetActions.tsx
@@ -1,8 +1,16 @@
-import { WrenchScrewdriverIcon, FlagIcon, AdjustmentsHorizontalIcon, TrashIcon, Cog6ToothIcon } from '@heroicons/react/24/outline'
-import { ButtonGroup, Button, Tooltip } from '@nextui-org/react'
-import React from 'react'
+import { WrenchScrewdriverIcon, FlagIcon, AdjustmentsHorizontalIcon, TrashIcon, Cog6ToothIcon , PencilSquareIcon, LinkIcon} from '@heroicons/react/24/outline'
+import { ButtonGroup, Button, Tooltip, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from '@nextui-org/react'
+import React, { useState } from 'react'
+import ReportWorkorder from './ReportWorkorder';
+import LinkedWorkorders from './LinkedWorkorders';
+import assets from '../assets.json'
+
+//TODO: add props to ReportWorkorder
 
 export default function LightingAssetActions() {
+
+    const iconClasses = "text-xs text-default-500 pointer-events-none flex-shrink-0";
+
   return (
     <ButtonGroup size="sm" className='grid lg:grid-cols-5 md:grid-cols-2'>
         <div className="px-1">
@@ -11,24 +19,34 @@ export default function LightingAssetActions() {
             </Tooltip>
         </div>
         <div className="px-1">
-            <Tooltip content="Shedule Maintanence">
-                <Button size="sm" className="px-1 bg-primary-500" isIconOnly><WrenchScrewdriverIcon/></Button>
-            </Tooltip>
+            <ReportWorkorder/>
         </div>
         <div className="px-1">
-            <Tooltip content="Control Lighting Asset">
-                <Button size="sm" className="px-1  bg-primary-500" isIconOnly><AdjustmentsHorizontalIcon/></Button>
-            </Tooltip>
+            <LinkedWorkorders/>
         </div>
         <div className="px-1">
-            <Tooltip content="Remove Digital Twin">
-                <Button size="sm" className="px-1 bg-primary-500" isIconOnly><TrashIcon/></Button>
-            </Tooltip>
-        </div>
-        <div className="px-1">
-            <Tooltip content="Settings">
-                <Button size="sm" className="px-1  bg-primary-500" isIconOnly><Cog6ToothIcon/></Button>
-            </Tooltip>
+              <Dropdown>
+                  <DropdownTrigger>
+                      <Button size="sm" className="px-1  bg-primary-500" isIconOnly><Cog6ToothIcon /></Button>
+                  </DropdownTrigger>
+                  <DropdownMenu variant="faded">
+                      <DropdownItem
+                          key="edit"
+                          //startContent={<PencilSquareIcon className={iconClasses} />} The startContent is not working as expected so, commenting it for now
+                      > Edit digital twin
+                      </DropdownItem>
+                      <DropdownItem
+                          key="control"
+                          //startContent={<AdjustmentsHorizontalIcon className={iconClasses} />}
+                      > Remotely control
+                      </DropdownItem>
+                      <DropdownItem
+                          key="delete"
+                          //startContent={<TrashIcon className={iconClasses} />}
+                      > Delete digital twin
+                      </DropdownItem>
+                  </DropdownMenu>
+              </Dropdown>
         </div>
     </ButtonGroup>
   )
