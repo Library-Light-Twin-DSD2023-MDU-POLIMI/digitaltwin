80306fdf7a3ec337230a49eaf01f6e6d76b6b735
heshani-wick <hwe22001@student.mdu.se>
2023-12-12 13:53:28 +0100

    update

--- frontend/src/app/components/LightingAssetHeader.tsx
+++ frontend/src/app/components/LightingAssetHeader.tsx
@@ -30,7 +30,6 @@ export default function LightingAssetHeader() {
           </div>
           <LightingAssetActions />
         </div>
-        <LightingAssetActions />
       </div>
       <StatusSummary />
     </div>
