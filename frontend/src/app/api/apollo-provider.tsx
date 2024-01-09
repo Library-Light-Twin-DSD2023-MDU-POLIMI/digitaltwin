d64568233966d4290a9f4be640574cc89e448059
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2024-01-09 12:12:20 +0100

    Feat: Apolloclient

---
+++ frontend/src/app/api/apollo-provider.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { ApolloLink, HttpLink } from "@apollo/client";
+import {
+  NextSSRApolloClient,
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "https://lcqhi8sll4.execute-api.eu-north-1.amazonaws.com/",
+    fetch,
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+export function ApolloWrapper({ children }: React.PropsWithChildren) {
+  return (
+    <ApolloNextAppProvider makeClient={makeClient}>
+      {children}
+    </ ApolloNextAppProvider>
+  );
+}
