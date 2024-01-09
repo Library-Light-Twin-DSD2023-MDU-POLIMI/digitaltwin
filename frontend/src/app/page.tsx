91e300c7665d029794a4435243aab8f20e925855
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2024-01-09 10:56:20 +0100

    Feat: connected frontend to backend in dashboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,8 +5,14 @@ import DigitalTwin from './components/digitalTwin'
 import * as userData from './assets.json'
 import * as digitalTwin from './digitalTwin.json'
 import { MagnifyingGlassIcon, PlusIcon } from '@heroicons/react/24/solid'
-import { Button, Input, Select, SelectItem } from '@nextui-org/react'
+import { Button, Input, Select, SelectItem, Textarea, useDisclosure } from '@nextui-org/react'
 import AddTwin from './components/addTwin'
+import AddTwinModal from './components/AddTwinModal'
+import { useMutation, useQuery } from '@apollo/client'
+import { ADD_LIGHTING_ASSET } from './_graphQL/Mutations'
+import { AddLightingAssetInput } from '@/utils/typeDefs'
+import {GET_LIGHTING_ASSETS} from './_graphQL/Queries'
+
 
 interface Asset {
   uid: string
@@ -20,6 +26,7 @@ interface Asset {
     section: string
     area: string
   }
+  cilLevel: number
 }
 
 interface DigitalTwinAsset {
@@ -59,103 +66,128 @@ interface DigitalTwinData {
 }
 
 const Home: React.FC = () => {
-  const [searchQuery, setSearchQuery] = useState('')
 
-  const data: AssetData = userData
+  
+  //Popup functionality for digital twin modal.
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const onOpen = () => setIsOpen(true);
+  const onClose = () => setIsOpen(false);
+
+  //Create addDigital Twin mutation
+  const [addDigitalTwin] = useMutation(ADD_LIGHTING_ASSET);
+  
+  
+  //Handle add digital twin
+  const onAddTwin = (uid: string, currentStatus: string, predictiveStatus: {status: string, predictedTime: Date}, type: string, cilLevel: number , location: {floor: number, section: string, area: string}) => {
+     {
+      let input: AddLightingAssetInput = {
+        uid: uid, 
+        currentStatus: currentStatus,
+        predictiveStatus: {
+          status: predictiveStatus.status,
+          predictedTime: predictiveStatus.predictedTime
+        },
+        type: type,
+        cilLevel: cilLevel,
+        location: {
+          floor: location.floor,
+          section: location.section,
+          area: location.area
+        }
+      };
+      addDigitalTwin({
+        variables: {
+          input: input,
+        },
+      });
+    }
+  };
+  
+  const { loading, error, data } = useQuery(GET_LIGHTING_ASSETS, {
+    variables: {
+      "input": {
+        "limit": 0,
+        "offset": 0,
+        "searchText": ""
+      },
+      "filter": {
+        "currentStatus": null,
+        "lightingType": null,
+        "location": {
+          "area": "",
+          "floor": 0,
+          "section": ""
+        },
+        "predictedStatus": null
+      }
+    }
+  });
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  
+    // Use data to render yor UI
+    console.log(JSON.stringify(data) + 'assets');
+  
+ /*  const [searchQuery, setSearchQuery] = useState('') */
+
+
   const digitaltwindata: DigitalTwinData = digitalTwin
 
-  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+ /*  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(event.target.value.toLowerCase())
   }
-
-  const filteredAssets = userData.Assets.filter(asset =>
+ */
+  /* const filteredAssets = userData.Assets.filter(asset =>
     asset.location.area.toLowerCase().includes(searchQuery)
-  )
-
-  const [showAddTwin, setShowAddTwin] = useState(false)
-  const toggleAddTwin = () => {
-    setShowAddTwin(!showAddTwin)
-  }
-
+  ) */
   return (
     <>
-      <div className="flex flex-col">
+      <div className="flex flex-col w-screen content-center ml-[100px]">
         <div
-          className="flex flex-col gap-4 bg-slate-300 p-7 rounded-lg"
+          className="flex flex-row gap-4 bg-slate-300 p-7 rounded-lg"
           style={{
-            marginLeft: '50px',
+            marginLeft: '100px',
             marginTop: '50px',
             marginRight: '60px',
             marginBottom: '40px',
+            width: '400px',
+            alignItems: 'center'
           }}
         >
           <Input
             placeholder="Search"
             className="max-w-xs"
-            onChange={handleSearchChange}
-            startContent={<MagnifyingGlassIcon className="h-7 w-7" />}
-          />
-          <div
-            className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4"
-            style={{ width: '100%', maxWidth: '1000px' }}
+          /*   onChange={handleSearchChange} */
+            startContent={<MagnifyingGlassIcon className="h-7 w-7" 
+            />} />
+          <Button
+            isIconOnly
+            style={{ backgroundColor: '#07C075', color: 'white' }}
+            onClick={onOpen}
           >
-            <Select label="Floor" className="max-w-xs">
-              <SelectItem key={''}>Floor 0</SelectItem>
-              <SelectItem key={''}>Floor 1</SelectItem>
-              <SelectItem key={''}>Floor 2</SelectItem>
-              <SelectItem key={''}>Floor 3</SelectItem>
-            </Select>
+            <PlusIcon />
+          </Button>
 
-            <Select label="Section" className="max-w-xs">
-              <SelectItem key={''}>Section 1</SelectItem>
-              <SelectItem key={''}>Section 2</SelectItem>
-              <SelectItem key={''}>Section 3</SelectItem>
-            </Select>
-
-            <Select label="Current status" className="max-w-xs">
-              <SelectItem key={''}>Good</SelectItem>
-              <SelectItem key={''}>Warning</SelectItem>
-              <SelectItem key={''}>Broke</SelectItem>
-            </Select>
-
-            <Select label="Predicted status" className="max-w-xs">
-              <SelectItem key={''}>Good</SelectItem>
-              <SelectItem key={''}>Warning</SelectItem>
-              <SelectItem key={''}>Broke</SelectItem>
-            </Select>
-
-            <Select label="Critical Level" className="max-w-xs">
-              <SelectItem key={''}>I</SelectItem>
-              <SelectItem key={''}>II</SelectItem>
-            </Select>
-
-            <Button
-              isIconOnly
-              style={{ backgroundColor: '#07C075', color: 'white' }}
-              onPress={toggleAddTwin}
-            >
-              <PlusIcon />
-            </Button>
-          </div>
-
-          {showAddTwin && <AddTwin />}
+          <AddTwinModal
+            onOpen={onOpen}
+            isOpen={isOpen}
+            onClose={onClose}
+            onAddTwin={onAddTwin} />
         </div>
-        <div className="flex flex-col" style={{ marginLeft: '50px' }}>
-          {filteredAssets.map(asset => {
-            const twinData = digitaltwindata.DigitalTwins.find(
-              dt => dt.assetId === asset.uid
-            )
-            return twinData ? (
-              <DigitalTwin
-                key={asset.uid}
-                asset={asset}
-                digitalTwin={twinData}
-              />
-            ) : null
-          })}
-        </div>
-      </div>
-    </>
+        <div style={{
+            marginLeft: '50px',
+            marginTop: '50px',
+            marginRight: '60px',
+            marginBottom: '40px',
+            width: '800px',
+          }}>
+
+      {data.lightingAssets.map((asset: Asset, index: number) => (
+        <DigitalTwin asset={asset} />
+      ))}
+    </div>
+      </div></>
   )
 }
 
